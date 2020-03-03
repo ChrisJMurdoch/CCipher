b121849d0cd1ca2218fa3de0664a0cef1e663312
ChrisJMurdoch <christopherjmurdoch@gmail.com>
2020-03-03 20:51:42 +0000

    Reading header and raster into struct

--- include/main.h
+++ include/main.h
@@ -1,5 +1,24 @@
 
 #pragma once
 
+struct Pixel
+{
+    int r;
+    int g;
+    int b;
+};
+struct PPM {
+    int height;
+    int width;
+    int maxval;
+    struct Pixel raster[];
+};
+
 char * readWord(FILE *fp);
 void nextLine(FILE *fp);
+void showPPM(struct PPM *im);
+void printRaster(struct PPM *im);
+
+struct PPM * getPPM(FILE *f);
+struct PPM * encode(struct PPM *im, char *message, unsigned int mSize, unsigned int secret);
+char * decode(struct PPM *im, unsigned int secret);
--- src/main.c
+++ src/main.c
@@ -1,42 +1,64 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "../include/main.h"
 
 int main(int argc, char *argv[])
 {
-    printf("\nOpening file..\n");
+    // Open file
     FILE *fp = fopen("./resources/home-cat.ppm", "r");
+    if(fp == NULL)
+        return -1;
 
-    printf("\nReading header..\n");
-    printf( "%s%s%s", "PPM code: ", readWord(fp), "\n" );
-    nextLine(fp); // Skip ppm comment - temporary fix
-    printf( "%s%s%s", "Width: ", readWord(fp), "\n" );
-    printf( "%s%s%s", "Height: ", readWord(fp), "\n" );
-    printf( "%s%s%s", "MaxVal: ", readWord(fp), "\n" );
+    // Read header code
+    if ( strcmp( readWord(fp), "P3" ) != 0)
+        return -1;
+    
+    // Skip ppm comment - temporary fix
+    nextLine(fp);
 
-    printf("\nReading raster..\n");
-    int count = 0;
+    // Read and parse header values
+    int width = atoi( readWord(fp) );
+    int height = atoi( readWord(fp) );
+    int maxval = atoi( readWord(fp) );
+
+    // Create struct
+    struct PPM *ppm = malloc( sizeof(struct PPM) + sizeof(struct Pixel) * width*height );
+    ppm->width = width;
+    ppm->height = height;
+    ppm->maxval = maxval;
+
+    // Read raster
+    int i = 0;
     bool done = false;
     while (!done)
     {
-        char *word = readWord(fp);
-        if (word[0] != '\0')
-            count++;
+        int r = atoi(readWord(fp));
+        int g = atoi(readWord(fp));
+        char *blue = readWord(fp);// Save last string to check for EOF
+        int b = atoi(blue);
+        if (blue[0] != 0) // Empty string after EOF
+            ppm->raster[i++] = (struct Pixel) { r, g, b };
         else
             done = true;
     }
-    printf("Raster size: %d.", count);
-    printf("\n\nDone.\n");
+
+    // Display
+    showPPM(ppm);
+
+    printf("Program exiting successfully.\n");
+    return 0;
 }
 
 char * readWord(FILE *fp)
 {
     static char word[100];
-    int n = 0;
-    char character;
     // Append letters to array til whitespace reached
+    char character;
+    int n = 0;
     while ( (character = (char)fgetc(fp)) != ' ' && character != '\n' && character != EOF )
         word[n++] = character;
     // End string
@@ -46,7 +68,25 @@ char * readWord(FILE *fp)
 
 void nextLine(FILE *fp)
 {
-    while (fgetc(fp) != '\n')
-        printf(" "); // This dummy expresssion seems to cut the compile and pre-run time by ~90% - No idea why
+    while (fgetc(fp) != '\n');
+}
+
+void showPPM(struct PPM *ptr)
+{
+    printf("\nPPM Structure\n");
+    printf("%s%d\n", "Width: ", ptr->width);
+    printf("%s%d\n", "Height: ", ptr->height);
+    printf("%s%d\n", "MaxVal: ", ptr->maxval);
+    //printRaster(ptr);
     printf("\n");
 }
+
+void printRaster(struct PPM *ptr)
+{
+    int i =0;
+    for ( i=0; i<ptr->width*ptr->height; i++ )
+    {
+        printf( "R:%d, G:%d, B:%d\n", ptr->raster[i].r, ptr->raster[i].g, ptr->raster[i].b );
+    }
+}
+
